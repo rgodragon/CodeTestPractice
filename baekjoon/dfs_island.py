a7aa775334abb985eaff27fcd13d4c3a9ac81ba6
rgodragon <rgodragon@gmail.com>
2023-02-13 11:29:38 +0900

    baekjoon 4963

--- baekjoon/dfs_island.py
+++ baekjoon/dfs_island.py
@@ -1,64 +1,36 @@
-w,h = map(int, input().split())
-result=[]
+import sys
+sys.setrecursionlimit(10000)
 
-while w!=0 and h!=0:
+def dfs(x,y):
+    if x<0 or x>=h or y<0 or y>=w:
+        return False
+    if graph[x][y]==1:
+        graph[x][y]=0
+        dfs(x+1,y)
+        dfs(x-1,y)
+        dfs(x,y+1)
+        dfs(x,y-1)
+        dfs(x+1,y+1)
+        dfs(x+1,y-1)
+        dfs(x-1,y+1)
+        dfs(x-1,y-1)
+        return True
+    return False
+    
+
+while True:
+    w,h = map(int, input().split())
+    if w==0 and h==0:
+        break
     graph=[]
     num=0
 
     for _ in range(h):
         graph.append(list(map(int, input().split())))
+    # visited=[[0]*w for _ in range(h)]   
 
-    def dfs(x,y):
-        if x<0 or x>=h or y<0 or y>=w:
-            return False
-        if graph[x][y]==1:
-            graph[x][y]=0
-            dfs(x+1,y)
-            dfs(x-1,y)
-            dfs(x,y+1)
-            dfs(x,y-1)
-            dfs(x+1,y+1)
-            dfs(x+1,y-1)
-            dfs(x-1,y+1)
-            dfs(x-1,y-1)
-            return True
-        return False
-    
-    # print(graph)
-    for i in range(w):
-        for j in range(h):
+    for i in range(h): # h랑 w 틀린거임
+        for j in range(w):
             if dfs(i,j) :
                 num+=1
-    result.append(num)
-    w,h = map(int, input().split())
-
-for i in result:
-    print(i)
-
-'''
-1 1
-0
-2 2
-0 1
-1 0
-3 2
-1 1 1
-1 1 1
-5 4
-1 0 1 0 0
-1 0 0 0 0
-1 0 1 0 1
-1 0 0 1 0
-5 4
-1 1 1 0 1
-1 0 1 0 1
-1 0 1 0 1
-1 0 1 1 1
-5 5
-1 0 1 0 1
-0 0 0 0 0
-1 0 1 0 1
-0 0 0 0 0
-1 0 1 0 1
-0 0
-'''
+    print(num)
